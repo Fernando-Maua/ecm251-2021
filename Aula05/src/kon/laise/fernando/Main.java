1066c50897d9f19f29e90198ca0a17df9369223f
Fernando <19.01336-0@maua.br>
2021-03-15 09:58:15 -0300

    Criação da classe Sistema

---
+++ Aula05/src/kon/laise/fernando/Main.java
@@ -0,0 +1,8 @@
+package kon.laise.fernando;
+
+public class Main {
+    public static void main(String[] args) {
+        Sistema sistema = new Sistema();
+        sistema.executar();
+    }
+}
---
+++ Aula05/src/kon/laise/fernando/Sistema.java
@@ -0,0 +1,8 @@
+package kon.laise.fernando;
+
+public class Sistema {
+    public void executar(){
+        System.out.println("Aqui!");
+    }
+}
+
