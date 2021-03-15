815e3d5dd2fbd096613fd93920397fcd7e58157f
Fernando <19.01336-0@maua.br>
2021-03-15 10:14:31 -0300

    Criação do construtor do Sistema

--- Aula05/src/kon/laise/fernando/Cliente.java
+++ Aula05/src/kon/laise/fernando/Cliente.java
@@ -1,14 +1,5 @@
 package kon.laise.fernando;
 
 public class Cliente {
-    String titular;
-    String cpf;
 
-    @Override
-    public String toString() {
-        return "Cliente{" +
-                "titular='" + titular + '\'' +
-                ", cpf='" + cpf + '\'' +
-                '}';
-    }
 }
--- Aula05/src/kon/laise/fernando/Sistema.java
+++ Aula05/src/kon/laise/fernando/Sistema.java
@@ -1,8 +1,32 @@
 package kon.laise.fernando;
+import java.util.Scanner;
 
 public class Sistema {
+    private boolean executarSistema;
+    private Scanner scanner;
+
     public void executar(){
-        System.out.println("Aqui!");
+        int opcao;
+        while(executarSistema){
+            exibirMenu();
+            opcao = scanner.nextInt();
+        }
     }
+
+    public Sistema() {
+        this.executarSistema = true;
+        this.scanner = new Scanner(System.in); //Cria um scanner para o teclado 
+    }
+
+    private void exibirMenu() {
+        System.out.println("Bem vindo ao MauaBank");
+        System.out.println("1 - Visualizar Saldo");
+        System.out.println("2 - Depositar dinheiro");
+        System.out.println("3 - Sacar dinheiro");
+        System.out.println("4 - Transferir dinheiro");
+        System.out.println("5 - Pagar conta (título)");
+        System.out.println("0 - Encerrar Sistema");
+    }
+
 }
 
